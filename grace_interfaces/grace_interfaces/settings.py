9b4f6cdc036ca9cafabe74b53f548d71ceadc1fd
gracekturner <gracekt@princeton.edu>
2017-08-28 15:53:36 -0400

    Update settings.py

--- grace_interfaces/grace_interfaces/settings.py
+++ grace_interfaces/grace_interfaces/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'z-_n_$^nxmq9)3&=6^+t4kgv(ug6afa!izg^d8u6^!m_0j*x#e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gracekt.pythonanywhere.com']
 
 
 # Application definition
